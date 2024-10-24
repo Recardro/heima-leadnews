a62fe93192dc497a3233c46451e0809c9f9d1f5d
Recordro <1767726918@qq.com>
2024-10-24 15:33:17 +0800

    Day3-07

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/WemediaApplication.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/WemediaApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.heima.wemedia.mapper")
-public class WemediaApplication {
+public class  WemediaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(WemediaApplication.class,args);
